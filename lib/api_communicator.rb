054829eff57be33bc320a37eceb1d3e83e72938a
Steven Nunez <steven.nunez@gmail.com>
2017-02-08 14:29:44 -0500

    Before Break

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,29 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+def get_character_movies_from_api(character_name)
+  character_name = "han solo"
+  url = 'http://www.swapi.co/api/people/'
+  all_characters = []
+  begin
+    characters_raw = RestClient.get(url).body
+    result = JSON.parse(characters_raw)
+    all_characters += result["results"]
+    url = result["next"]
+  end while url
+  # look up the character and stop when you find it
+  found_character = all_characters.find do |character|
+    name = character["name"]
+    name.downcase == character_name
+  end
+  binding.pry
+
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
-  # puts out a welcome message here!
+  puts "*" * 30
+  puts "Welcome to the dopest star wars search thing evar"
+  puts "*" * 30
 end
 
 def get_character_from_user
   puts "please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
+  # gets.chomp.downcase
 end
